8b4216e49efef0d6f89496aef57e85b66965787d
Avinash <avinash@Avinashs-MacBook-Pro-2.local>
2022-06-14 01:38:42 +0530

    my price and id changes

--- src/app/classes/comments.ts
+++ src/app/classes/comments.ts
@@ -1,7 +1,6 @@
 export class comments {
-  postId?: number = 0;
   id?: number = 0;
-  name?: string = ' ';
-  email?: string = ' ';
-  body?: string = ' ';
+  name?: string = " ";
+  description?: string = " ";
+  price?: string = " ";
 }
